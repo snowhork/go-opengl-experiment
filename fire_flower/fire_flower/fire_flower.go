e232054d1954a95664f204bd57b20e818f45aa37
kyoh.maruo <romio_cinderella@yahoo.co.jp>
2018-04-21 15:05:09 +0900

    subsphere

--- fire_flower/fire_flower/fire_flower.go
+++ fire_flower/fire_flower/fire_flower.go
@@ -25,20 +25,21 @@ func NewFireFlower(
 	vertexCount int) *FireFlower {
 
 	phi := float32((1+math.Sqrt2)/2.0)
+	SphereR := float32(math.Sqrt(PHI*(math.Sqrt(5))))
 
 	points := []*basic.Point{
-		basic.NewPoint(1, phi, 0),
-		basic.NewPoint(1, -phi, 0),
-		basic.NewPoint(-1, -phi, 0),
-		basic.NewPoint(-1, phi, 0),
-		basic.NewPoint(0, 1, phi),
-		basic.NewPoint(0, 1, -phi),
-		basic.NewPoint(0, -1, -phi),
-		basic.NewPoint(0, -1, phi),
-		basic.NewPoint(phi, 0, 1),
-		basic.NewPoint(-phi, 0, 1),
-		basic.NewPoint(-phi, 0, -1),
-		basic.NewPoint(phi, 0, -1),
+		basic.NewPoint(1, phi, 0).Mult(1.0/SphereR),
+		basic.NewPoint(1, -phi, 0).Mult(1.0/SphereR),
+		basic.NewPoint(-1, -phi, 0).Mult(1.0/SphereR),
+		basic.NewPoint(-1, phi, 0).Mult(1.0/SphereR),
+		basic.NewPoint(0, 1, phi).Mult(1.0/SphereR),
+		basic.NewPoint(0, 1, -phi).Mult(1.0/SphereR),
+		basic.NewPoint(0, -1, -phi).Mult(1.0/SphereR),
+		basic.NewPoint(0, -1, phi).Mult(1.0/SphereR),
+		basic.NewPoint(phi, 0, 1).Mult(1.0/SphereR),
+		basic.NewPoint(-phi, 0, 1).Mult(1.0/SphereR),
+		basic.NewPoint(-phi, 0, -1).Mult(1.0/SphereR),
+		basic.NewPoint(phi, 0, -1).Mult(1.0/SphereR),
 	}
 
 	baseFace := []*face{
@@ -118,19 +119,38 @@ func (flower *FireFlower) Update() {
 }
 
 func (flower *FireFlower) Draw() {
-	points := make([]float32, 7*len(flower.lines)*flower.vertexCount, 7*len(flower.lines)*flower.vertexCount)
+	array := make([]float32, 7*3*len(flower.faces), 7*3*len(flower.faces))
 
-	for i := range flower.lines {
-		for j := 0; j < 7*flower.vertexCount; j++ {
-			points[i*7*flower.vertexCount + j] = flower.lines[i].points[j]
-		}
+	for i, face := range flower.faces {
+		array[i*21 + 7*0 + 0] = face.points[0].X
+		array[i*21 + 7*0 + 1] = face.points[0].Y
+		array[i*21 + 7*0 + 2] = face.points[0].Z
+		array[i*21 + 7*0 + 3] = 1.0
+		array[i*21 + 7*0 + 4] = 0.0
+		array[i*21 + 7*0 + 5] = 0.0
+		array[i*21 + 7*0 + 6] = 1.0
+		array[i*21 + 7*1 + 0] = face.points[1].X
+		array[i*21 + 7*1 + 1] = face.points[1].Y
+		array[i*21 + 7*1 + 2] = face.points[1].Z
+		array[i*21 + 7*1 + 3] = 1.0
+		array[i*21 + 7*1 + 4] = 0.0
+		array[i*21 + 7*1 + 5] = 0.0
+		array[i*21 + 7*1 + 6] = 1.0
+		array[i*21 + 7*2 + 0] = face.points[2].X
+		array[i*21 + 7*2 + 1] = face.points[2].Y
+		array[i*21 + 7*2 + 2] = face.points[2].Z
+		array[i*21 + 7*2 + 3] = 1.0
+		array[i*21 + 7*2 + 4] = 0.0
+		array[i*21 + 7*2 + 5] = 0.0
+		array[i*21 + 7*2 + 6] = 1.0
+
 	}
 
-	VAO := makeVao(points)
+	VAO := makeVao(array)
 	gl.BindVertexArray(VAO)
 
-	for i := range flower.lines {
-		gl.DrawArrays(gl.LINE_STRIP, int32(i*flower.vertexCount), int32(flower.vertexCount))
+	for i := range flower.faces {
+		gl.DrawArrays(gl.LINE_LOOP, int32(i*3), 3)
 	}
 }
 
